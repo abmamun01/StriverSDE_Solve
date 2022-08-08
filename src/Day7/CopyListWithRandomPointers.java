60ce2bbca8fd2f3aa4c14608ddbe58e2877cae38
abmamun01 <aamamun25000@gmail.com>
2022-08-08 11:40:38 +0600

    Updated

--- src/Day7/CopyListWithRandomPointers.java
+++ src/Day7/CopyListWithRandomPointers.java
@@ -1,4 +1,44 @@
 package Day7;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyListWithRandomPointers {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+
+        Map<Node, Node> map = new HashMap<>();
+        Node temp = head;
+
+        // make a copy LL
+        while (temp != null) {
+            Node copy = new Node(temp.val);
+            //original node r address onujay copy node r address store korbo
+            map.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+
+        // making connection
+        // traverse again
+        // Deep Copy
+        temp = head;
+        while (temp != null) {
+            // temp r copy r next == temp r next
+            map.get(temp).next = map.get(temp.next);
+            map.get(temp).random = map.get(temp.random);
+            temp=temp.next;
+        }
+        return map.get(head);
+    }
 }
---
+++ src/Day7/CopyListWithRandomPointersNoExtraSpace.java
@@ -0,0 +1,46 @@
+package Day7;
+
+public class CopyListWithRandomPointersNoExtraSpace {
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyList(Node head) {
+        Node temp = head;
+        // jate temp k samne barale o location thik thake , tai next use korechi
+        Node next = head.next;
+        while (temp != null) {
+
+            Node copy = new Node(temp.val);
+            // temp r next jora dibo
+            temp.next = copy;
+            // now temp will become n
+            temp = next;
+            //next will become next.next
+            if (next != null) next = next.next;
+        }
+    }
+
+    void handleRandom(Node head) {
+        Node temp = head;
+        while (temp!=null){
+            // temp j next seta holo copy of temp
+
+        }
+    }
+
+    Node copyRandomList(Node head) {
+        //base case
+        if (head == null) return null;
+        copyList(head);
+    }
+}
