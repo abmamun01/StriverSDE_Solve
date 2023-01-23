a42c83a6fa08534a7120e1236fae19ddfc723c9e
abmamun01 <aamamun25000@gmail.com>
2023-01-23 13:00:02 +0600

    NumberOfProvinces

--- src/Graph/NumberOfProvinces.java
+++ src/Graph/NumberOfProvinces.java
@@ -3,51 +3,29 @@ package Graph;
 public class NumberOfProvinces {
     public int findCircleNum(int[][] isConnected) {
 
-        boolean visited[] = new boolean[isConnected.length];
+        int length = isConnected.length;
+        boolean visited[] = new boolean[length];
         int count = 0;
-        for (int i = 0; i < isConnected.length; i++) {
+
+        for (int i = 0; i < length; i++) {
+
             if (!visited[i]) {
-                count++;
-                System.out.println();
-                dfs(i, isConnected, visited);
-
-            }
-        }
-
-        return count;
-    }
-
-
-    void dfs(int node, int[][] adjList, boolean[] visited) {
-
-        visited[node] = true;
-        for (int i = 0; i < adjList.length; i++) {
-            if (visited[i] == false) {
-                dfs(i, adjList, visited);
-            }
-
-        }
-
-    }
-
-    public void dfs(int[][] M, boolean[] visited, int i) {
-        for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == false) {
-                visited[j] =true;
-                dfs(M, visited, j);
-            }
-        }
-    }
-    public int findCircleNums(int[][] M) {
-        boolean[] visited = new boolean[M.length];
-        int count = 0;
-        for (int i = 0; i < M.length; i++) {
-            if (visited[i] == false) {
-                dfs(M, visited, i);
+                dfsTraversal(i, visited, isConnected);
                 count++;
             }
         }
         return count;
+    }
+
+    private void dfsTraversal(int givenNode, boolean[] visited, int[][] isConnected) {
+        visited[givenNode] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[givenNode][i] == 1 && !visited[i]) {
+                System.out.println(" Given Node "+isConnected[givenNode][i]);
+                dfsTraversal(i, visited, isConnected);
+            }
+        }
+
     }
 
 }
