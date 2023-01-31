2a453bdf1dff5747c7052f69a7910cc064767764
Abdullah Al Mamun <aamamun25000@gmail.com>
2023-01-31 20:07:04 +0600

    RottenOranges

--- src/Graph/RottenOranges.java
+++ src/Graph/RottenOranges.java
@@ -4,70 +4,69 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class RottenOranges {
+
+
     public int orangesRotting(int[][] grid) {
 
         int n = grid.length;
         int m = grid[0].length;
+
+        int count = 0;
         int countFresh = 0;
 
+        int[][] result = grid;
+
+        boolean visited[][] = new boolean[n][m];
+
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1};
+
         Queue<Pair> queue = new LinkedList<>();
-        int visited[][] = new int[n][m];
+
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
+                    visited[i][j] = true;
                     queue.add(new Pair(i, j, 0));
-                    visited[i][j] = 2;
-                } else {
-                    visited[i][j] = 0;
                 }
 
+                // countring fresh tomato so that these fresh tomato will be rotten then rotten count++
+                // if rotten == fresh then all fresh are rotten other wise no
                 if (grid[i][j] == 1) countFresh++;
             }
         }
 
-
         int time = 0;
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, 1, 0, -1};
-        int count = 0;
-
 
         while (!queue.isEmpty()) {
-            int row = queue.peek().row;
-            int col = queue.peek().column;
-            int tme = queue.peek().time;
 
-            time = Math.max(tme, time);
-            queue.poll();
+            int inRow = queue.peek().row;
+            int inCol = queue.peek().column;
+            int inTime = queue.peek().time;
 
+            time = Math.max(time, inTime);
+            queue.remove();
 
             for (int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int nCol = col + delCol[i];
+                int nRow = inRow + delRow[i];
+                int nCol = inCol + delCol[i];
 
-                if (nrow >= 0 && nrow < n && nCol >= 0 && nCol < m && visited[nrow][nCol] == 0 && grid[nrow][nCol] == 1) {
+                if (nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length
+                        && !visited[nRow][nCol] && grid[nRow][nCol] == 1) {
 
-                    // akhan theke jekhane e move korbe 1 unit time barbe
-                    // karon tmi jei jaygay acho sekhane to 0 but ak pa agaile e 1 hoye
-                    // gelo thik temn ata jedike e e jabe 1unit hobe
-                    queue.add(new Pair(nrow, nCol, time + 1));
-                    visited[nrow][nCol] = 2;
-
-                    // ai queue method ta to hocce upore 1 pase 1 nice & right saide 1 ghor agabe
-                    // ar ata tokhon e agabe jokhon se kono fresh fruit tar sathe lagano thakbe
-                    /// jodi fruit ta dure hoy tahole fresh fruit fresh e thakbe
                     count++;
+                    queue.add(new Pair(nRow, nCol, inTime + 1));
+                    visited[nRow][nCol] = true;
 
                 }
             }
         }
 
-        // as question said that if there is even 1 fresh orange return -1
-        // so here is checking that
         if (count != countFresh) return -1;
         return time;
     }
+
 
     class Pair {
         int row;
